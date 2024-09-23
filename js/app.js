9aa613e0969252d29b54398f818ce05359cc5aa8
seffmeister <ismail.sefin@gmail.com>
2024-09-23 13:20:28 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let item1;
+let item2;
+let item3;
+
+function useItem(number) {
+  if(number === 1 && item1 !== "") {
+    Use(item1);item1 = "";   }
+}
+if(number === 2 && item2 !== "") {
+  Use(item2);item2 = "";
+}
+if(number === 3 && item3 !== "") {
+  Use(item3);item3 = "";   }
+// ...}
+
+
+
+//Create an array to count the occurrences of random numbers
+//Roll 10 000 times for a number between 0 and 10 by using:
+//  Math.floor(Math.random() * 11);
+//And count the number of times that you have rolled that specific number
+//Afterwards, print the result to the console.
+
+let randomNumberCount: = [0,0,0,0,0,0,0,0,0,0];
+
+randomNumberCount[0] += 1;
+
+console.log(randomNumberCount [0];
