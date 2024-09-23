56424b922ea50659ccbae98b9edfbfde42d468d0
seffmeister <ismail.sefin@gmail.com>
2024-09-23 14:29:50 +0200

    exercise in class

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-let item1;
-let item2;
-let item3;
-
-function useItem(number) {
-  if(number === 1 && item1 !== "") {
-    Use(item1);item1 = "";   }
-}
-if(number === 2 && item2 !== "") {
-  Use(item2);item2 = "";
-}
-if(number === 3 && item3 !== "") {
-  Use(item3);item3 = "";   }
-// ...}
-
-
 
 //Create an array to count the occurrences of random numbers
 //Roll 10 000 times for a number between 0 and 10 by using:
@@ -21,8 +5,18 @@ if(number === 3 && item3 !== "") {
 //And count the number of times that you have rolled that specific number
 //Afterwards, print the result to the console.
 
-let randomNumberCount: = [0,0,0,0,0,0,0,0,0,0];
+let randomNumber = [0,0,0,0,0,0,0,0,0,0];
 
-randomNumberCount[0] += 1;
+for (let i = 0; i < 100; i++) {
+  let randomNumber = getRandomInt(10);
+  getNumber [randomNumber] += 1;
+}
+  console.log(randomNumber);
 
-console.log(randomNumberCount [0];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+
